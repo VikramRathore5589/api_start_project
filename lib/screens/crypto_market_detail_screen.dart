848c372491e12ffa46012ba424c1c71998101a0f
Vikram <htchonda4@gmail.com>
2024-11-07 00:03:00 +0530

    create detail screen and change ui of crypto market screen

---
+++ lib/screens/crypto_market_detail_screen.dart
@@ -0,0 +1,57 @@
+import 'package:api_start_project/models/crypto_model.dart';
+import 'package:flutter/material.dart';
+
+class CryptoMarketDetailScreen extends StatelessWidget {
+  CryptoMarketDetailScreen({super.key, required this.cryptoModel});
+
+  final CryptoModel cryptoModel;
+  SizedBox size = const SizedBox(
+    height: 8,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Crypto Detail'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: CircleAvatar(
+                  radius: 50,
+                  child: Image.network(cryptoModel.image ?? 'No image')),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text("Name:${cryptoModel.name}"),
+            size,
+            Text("Current Price:${cryptoModel.currentPrice}"),
+            size,
+            Text("Market Cap:${cryptoModel.marketCap}.Cr"),
+            size,
+            Text("TotalVolume:${cryptoModel.totalVolume}"),
+            size,
+            Text("Symbol:${cryptoModel.symbol}"),
+            size,
+            Text("Max Supply:${cryptoModel.maxSupply}"),
+            size,
+            Text("Market Cap Rank:${cryptoModel.marketCapRank}"),
+            size,
+            Text("High 24Hr:${cryptoModel.high24h}"),
+            size,
+            Text("Low 24Hr:${cryptoModel.low24h}"),
+            size,
+            Text("PriceChange 24h:${cryptoModel.priceChange24h}"),
+            size,
+            Text(
+                "PriceChangePercentage 24h:${cryptoModel.priceChangePercentage24h}"),
+            size,
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/crypto_narket_screen.dart
+++ lib/screens/crypto_narket_screen.dart
@@ -1,6 +1,7 @@
 import 'package:api_start_project/models/crypto_model.dart';
 import 'package:api_start_project/providers/crypto_provider.dart';
 import 'package:api_start_project/screens/app_bar_widget.dart';
+import 'package:api_start_project/screens/crypto_market_detail_screen.dart';
 import 'package:api_start_project/ui_helper/ui_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -18,74 +19,96 @@ class _CryptoMarketScreenState extends State<CryptoMarketScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:const AppBarWidget(),
+      appBar: const AppBarWidget(),
       body: Consumer<CryptoProvider>(
         builder: (context, provider, child) {
           return FutureBuilder<List<CryptoModel>>(
-              future: provider.uiHelper.fetchCrypto(),
-              builder: (context, important) {
-                if (important.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (important.hasError) {
-                  return Text("${important.error}");
-                } else if (important.hasData) {
-                  var modelList = important.data!;
-                  return ListView.separated(
-                      itemBuilder: (context, index) {
-                        final model = modelList[index];
-                        return ListTile(
-                            leading:
-                                Image.network(model.image ?? 'No image found'),
-                            title: Text(model.name ?? 'No name found'),
-                            subtitle: Row(
-                              children: [
-                                Text(model.athChangePercentage.toString()),
-                              ],
-                            ),
-                            trailing: Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    const Icon(
-                                      Icons.currency_rupee,
-                                      size: 20,
+            future: provider.uiHelper.fetchCrypto(),
+            builder: (context, important) {
+              if (important.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (important.hasError) {
+                return Text("${important.error}");
+              } else if (important.hasData) {
+                var modelList = important.data!;
+                return ListView.separated(
+                    itemBuilder: (context, index) {
+                      final model = modelList[index];
+                      return ListTile(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      CryptoMarketDetailScreen(
+                                    cryptoModel: model,
+                                  ),
+                                ));
+                          },
+                          leading:
+                              Image.network(model.image ?? 'No image found'),
+                          title: Text(model.name ?? 'No name found'),
+                          subtitle: Row(
+                            children: [
+                              Text(
+                                model.marketCapRank!.toStringAsFixed(0),
+                                style: const TextStyle(color: Colors.blue),
+                              ),
+                              const SizedBox(
+                                width: 4,
+                              ),
+                              Text(model.symbol ?? 'No symbol found'),
+                              const SizedBox(
+                                width: 7,
+                              ),
+                              Text("${model.athChangePercentage.toString()}%"),
+                            ],
+                          ),
+                          trailing: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const Icon(
+                                    Icons.currency_rupee,
+                                    size: 20,
+                                    color: Colors.green,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  Text(
+                                    model.currentPrice!.toStringAsFixed(2),
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
                                       color: Colors.green,
                                     ),
-                                    const SizedBox(width: 5),
-                                    Text(
-                                      model.currentPrice!.toStringAsFixed(2),
-                                      style: const TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.green,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Text(
-                                  model.marketCap != null
-                                      ? '₹${model.marketCap!.toStringAsFixed(2)}'
-                                      : 'Market Cap: N/A',
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.grey,
                                   ),
+                                ],
+                              ),
+                              Text(
+                                model.marketCap != null
+                                    ? '₹${model.marketCap!.toStringAsFixed(2)}'
+                                    : 'Market Cap: N/A',
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey,
                                 ),
-                              ],
-                            ));
-                      },
-                      separatorBuilder: (context, index) {
-                        return const Divider();
-                      },
-                      itemCount: modelList.length);
-                } else {
-                  return const Text('No dta');
-                }
-              });
+                              ),
+                            ],
+                          ));
+                    },
+                    separatorBuilder: (context, index) {
+                      return const Divider();
+                    },
+                    itemCount: modelList.length);
+              } else {
+                return const Text('No data');
+              }
+            },
+          );
         },
       ),
     );
